b164426dd2d31e29093581db33973816696d7551
Alan Novianto <77398526+Mayuzumi05@users.noreply.github.com>
2023-12-19 14:43:11 +0700

    Export Success Ril

--- app/Exports/BarangExport.php
+++ app/Exports/BarangExport.php
@@ -19,19 +19,20 @@ class BarangExport implements FromCollection, WithHeadings
     public function headings(): array
     {
         return [
-            'material_number' => $row[1],
-            'nama_barang' => $row[2],
-            'range_pengukuran' => $row[3],
-            'satuan_pengukuran' => $row[4],
-            'deskripsi' => $row[5],
-            'kondisi' => $row[6],
-            'merk' => $row[7],
-            'tipe' => $row[8],
-            'jumlah Barang' => $row[9],
-            'id_satuan_barang' => $row[10],
-            'lokasi' => $row[11],
-            'created_at' => $row[12],
-            'updated_at' => $row[13],
+            'No.',
+            'Material Number',
+            'Nama Barang',
+            'Range Pengukuran',
+            'Satuan Pengukuran',
+            'Deskripsi',
+            'Kondisi',
+            'Merk',
+            'Tipe',
+            'Jumlah Barang',
+            'ID Satuan Barang',
+            'Lokasi',
+            'Created At',
+            'Updated At',
         ];
     }
 }
