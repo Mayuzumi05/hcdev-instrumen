bb657ac717d6534039220ef311659bb5c45cfecb
Alan Novianto <77398526+Mayuzumi05@users.noreply.github.com>
2024-02-22 10:48:12 +0700

    carbon set indo

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Carbon;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -22,5 +23,8 @@ class AppServiceProvider extends ServiceProvider
     {
         //
         Paginator::useBootstrap();
+        config(['app.locale' => 'id']);
+        Carbon::setlocale('id');
+        date_default_timezone_set('Asia/Jakarta');
     }
 }
