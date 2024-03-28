c26d747cbc28662707c0914d47010a6287576368
Alan Novianto <77398526+Mayuzumi05@users.noreply.github.com>
2024-03-28 07:16:33 +0700

    migration table barang

---
+++ database/migrations/2024_03_28_071107_barang.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('barang', function (Blueprint $table) {
+            $table->id();
+            $table->integer('material_number');
+            $table->string('nama_barang');
+            $table->string('range_pengukuran');
+            $table->string('satuan_pengukuran');
+            $table->string('deskripsi');
+            $table->string('kondisi');
+            $table->string('merk');
+            $table->string('tipe');
+            $table->integer('jumlah_barang');
+            $table->string('id_satuan_barang');
+            $table->integer('lokasi');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('barang');
+    }
+};
